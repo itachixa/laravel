0cecb484a7702b3cce33a9dd5b1665499853b324
itachixa <xaodin12@gmail.com>
2024-09-18 15:07:04 +0000

    I add readme

--- new-app/resources/views/Acceuil.blade.php
+++ new-app/resources/views/Acceuil.blade.php
@@ -74,8 +74,7 @@
     </style>
 </head>
 <body>
-    
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <nav class="navbar navbar-expand-lg navbar-blue bg-dark">
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
